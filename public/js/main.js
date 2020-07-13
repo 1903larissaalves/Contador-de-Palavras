e2c86bdfde5652e4871d1b05c498a8a1f0d46c69
Larissa Alves <1903larissaalves@gmail.com>
2020-07-12 23:46:38 -0300

    Contando palavras e caracteres digitados pelo usuario

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,15 @@ var numeroPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#quantidadePalavras").text(numeroPalavras);
 
+var campo = $("#campoDigitacao");
+campo.on("input", function(){
 
+    var conteudo = campo.val();
+
+    var quantidadePalvras = conteudo.split(/\s+/).length -1;
+    $("#contadorPalavras").text(quantidadePalvras);
+
+    var quantidadeCaracteres = conteudo.length;
+    $("#contadorCaracteres").text(quantidadeCaracteres);
+
+});
