349f0f04b1da13d7f04fdca9edbe63881557fb60
Larissa Alves <1903larissaalves@gmail.com>
2020-07-16 23:54:48 -0300

    Aplicando css

--- public/js/main.js
+++ public/js/main.js
@@ -40,6 +40,8 @@ function cronometro() {
         if(tempoRestante < 1){
             campo.attr("disabled", true);
             clearInterval(cronometro);
+            campo.addClass("campo-desativado");
+            //campo.toggleClass("campo-desativado"); --faz o add class e remove
         }
     },1000);
 
