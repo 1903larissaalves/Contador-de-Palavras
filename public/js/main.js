344913827daed315a82e7f203c74908237a5b1d3
Larissa Alves <1903larissaalves@gmail.com>
2020-07-16 23:04:57 -0300

    Opcao de reiniciar jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,40 @@
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#quantidadePalavras").text(numeroPalavras);
-
 var campo = $("#campoDigitacao");
-campo.on("input", function(){
+var tempoInicial = $("#tempoDigitacao").text();
 
-    var conteudo = campo.val();
-
-    var quantidadePalvras = conteudo.split(/\s+/).length -1;
-    $("#contadorPalavras").text(quantidadePalvras);
-
-    var quantidadeCaracteres = conteudo.length;
-    $("#contadorCaracteres").text(quantidadeCaracteres);
-
+$(function() {
+    cronometro();
+    inicializaContadores();
+    atualizaTamanhoFrase();
+    $("#reiniciar").click(reiniciarJogo);
 });
 
-var tempoRestante = $("#tempoDigitacao").text();
-campo.one("focus", function(){
-    var cronometro = setInterval(function(){
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numeroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#quantidadePalavras").text(numeroPalavras);
+}
+
+
+function inicializaContadores() {
+    campo.on("input", function(){
+
+        var conteudo = campo.val();
+    
+        var quantidadePalvras = conteudo.split(/\s+/).length -1;
+        $("#contadorPalavras").text(quantidadePalvras);
+    
+        var quantidadeCaracteres = conteudo.length;
+        $("#contadorCaracteres").text(quantidadeCaracteres);
+    
+    });
+}
+
+function cronometro() {
+    var tempoRestante = $("#tempoDigitacao").text();
+    
+    campo.one("focus", function(){
+        var cronometro = setInterval(function(){
         tempoRestante--;
         $("#tempoDigitacao").text(tempoRestante);
         if(tempoRestante < 1){
@@ -26,4 +42,15 @@ campo.one("focus", function(){
             clearInterval(cronometro);
         }
     },1000);
+
 });
+}
+
+function reiniciarJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contadorPalavras").text("0");
+    $("#contadorCaracteres").text("0");
+    $("#tempoDigitacao").text(tempoInicial);
+    cronometro();
+}
